89d38d4ec05a11569e94cb3f9b011b1bed82938a
João D'Amorim <joao.damorim@gmail.com>
2023-01-31 14:52:46 -0300

    Creating pages and routing

---
+++ src/pages/about.tsx
@@ -0,0 +1,7 @@
+function About() {
+    return (
+        <h1>Página de About</h1>
+    )
+}
+
+export default About
---
+++ src/pages/products.tsx
@@ -0,0 +1,7 @@
+function Products() {
+    return (
+        <h1>Página de Produtos</h1>
+    )
+}
+
+export default Products
